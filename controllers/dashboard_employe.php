c753ed3c07629444df504c76b57d658b42f5817a
juliettedef <juliette.defrance@outlook.fr>
2026-01-15 11:59:08 +0100

    correction fonction setStatusTask

--- controllers/dashboard_employe.php
+++ controllers/dashboard_employe.php
@@ -3,6 +3,7 @@
 require_once __DIR__ . '/../config/database.php';
 require_once __DIR__ . '/../includes/functions.php';
 require_once __DIR__ . '/../models/getTasksByUser.php';
+require_once __DIR__ . '/../models/setStatusTask.php';
 
 if (session_status() === PHP_SESSION_NONE) {
     session_start();
@@ -10,6 +11,16 @@ if (session_status() === PHP_SESSION_NONE) {
 
 if (!isset($_SESSION['user_role'], $_SESSION['user_id'])) {
     redirect('index.php?page=login');
+}
+
+if (isset($_POST['task_id'], $_POST['status'])) {
+    $taskId = (int)$_POST['task_id'];
+    $status = trim($_POST['status']);
+
+    setStatusTask($pdo, $taskId, (int)$_SESSION['user_id'], $status);
+
+    header('Location: index.php?page=employe');
+    exit;
 }
 
 $tasks = getTasksByUser($pdo, $_SESSION['user_id']);
